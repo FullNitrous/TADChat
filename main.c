034ea4975d99d4bc112372276ec8d50d044d9f33
FullNitrous <fullnitrous@gmail.com>
2018-10-08 21:57:24 +0200

    lol

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 int main(void)
 {
-  size_t read = -1;
-
-  printf("========================================\n");
-  printf("Terry A Davis random memory address chat\n");
-  printf("\n========================================\n\n");
+  int read = 0;
+  printf(
+  "  _________    ____  ________          __\n" 
+  " /_  __/   |  / __ \\/ ____/ /_  ____ _/ /_\n"
+  "  / / / /| | / / / / /   / __ \\/ __ `/ __/\n"
+  " / / / ___ |/ /_/ / /___/ / / / /_/ / /_\n"
+  "/_/ /_/  |_/_____/\\____/_/ /_/\\__,_/\\__/\n");
+  printf("Powered by the pure autism of FullNitrous\n");
+  printf("________________voidzehn_________________\n\n");
 
   printf("[MESSAGE] Enter amount of bytes to be read: ");
   while(scanf("%d", &read) != 1)
@@ -14,5 +20,17 @@ int main(void)
     printf("[ERROR, MESSAGE] Amount must be a number: ");
     while(getchar() != '\n');
   }
-  printf("lol: %d\n", read);
+  uint8_t* data = malloc(sizeof(uint8_t) * read);
+  for(int i = 0; i < read; i++)
+  {
+    printf("[%p] %c, 0x%x\n", (uintptr_t)(&data[i]), data[i], data[i]);
+  }
+  printf("\nString: \n");
+  for(int i = 0; i < read; i++)
+  {
+    putchar(data[i]);
+  }
+  printf("\n");
+  free(data);
+  return 0;
 }
